97329d9f043e78dbc78146109ce77e41caed85ee
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-01 00:04:11 +0300

    findByText

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('TEST APP', () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const helloElement = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(btn).toBeInTheDocument();
+    // expect(input).toBeInTheDocument();
+    expect(helloElement).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+    screen.debug();
+  });
+  test("renders learn react link", async() => {
+    render(<App />);
+    // const helloElement = screen.queryByText(/hello2/i);
+    // expect(helloElement).toBeNull();
+    const helloElement = await screen.findByText(/data/i);
+    expect(helloElement).toBeInTheDocument();
+  });
 });
